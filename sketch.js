d8186f98250b7f2bba43dae44bef527cdd0b7f5b
Jose Andrés Moncada <josemoncada87@hotmail.com>
2019-01-24 09:20:45 -0500

    simple mods

--- sketch.js
+++ sketch.js
@@ -39,10 +39,11 @@ var textoActivado;
 var imagenesActivado;
 var activarTextos;
 ////////////////////////////////////////
-
-
-
-
+var mazateVisible =  true;
+var huitzilinVisible =  true;
+var coateVisible =  true;
+var michinVisible =  true;
+////////////////////////////////////////////////////////////////
 
 var config = {
   apiKey: "AIzaSyDj1Ao1CQpZa0t5UO2V1LhgCM6kqE1xPcA",
@@ -63,7 +64,7 @@ function preload() {
 }
 
 function setup() {
-  var cnv = createCanvas(windowWidth, windowHeight*0.6);
+  var cnv = createCanvas(windowWidth, windowHeight * 0.6);
   cnv.parent('sketch-holder');
   imageMode(CENTER);
 
@@ -71,7 +72,7 @@ function setup() {
   this.puntosMazate = 0;
   this.puntosCoate = 0;
   this.puntosMichin = 0;*/
-  this.maximoPuntos = 30000;
+  this.maximoPuntos = 50000;
 
   this.wMazate = imgMazate.width;
   this.hMazate = imgMazate.height;
@@ -84,47 +85,92 @@ function setup() {
 
   setScale();
   setPositions();
+  filtrateAssignListener();
 }
 
 function setPositions() {
   this.xCoate = (windowWidth / 5) * 1;
-  this.yCoate = map(puntosCoate, 0, maximoPuntos, windowHeight*0.6, 250 * escala);
+  this.yCoate = map(puntosCoate, 0, maximoPuntos, windowHeight * 0.6, 250 * escala);
   this.xMazate = (windowWidth / 5) * 2;
-  this.yMazate = map(puntosMazate, 0, maximoPuntos, windowHeight*0.6, 250 * escala);
+  this.yMazate = map(puntosMazate, 0, maximoPuntos, windowHeight * 0.6, 250 * escala);
   this.xHuitzilin = (windowWidth / 5) * 3;
-  this.yHuitzilin = map(puntosHuitzilin, 0, maximoPuntos, windowHeight*0.6, 250 * escala);
+  this.yHuitzilin = map(puntosHuitzilin, 0, maximoPuntos, windowHeight * 0.6, 250 * escala);
   this.xMichin = (windowWidth / 5) * 4;
-  this.yMichin = map(puntosMichin, 0, maximoPuntos, windowHeight*0.6, 250 * escala);
+  this.yMichin = map(puntosMichin, 0, maximoPuntos, windowHeight * 0.6, 250 * escala);
 }
 
 function setScale() {
-  this.escala = (windowHeight*0.6 / imgMazate.height);
+  this.escala = (windowHeight * 0.6 / imgMazate.height);
   this.escala = map(escala, 1, 20, 0.5, 1.5);
 }
 
-function mousePressed() {
-  let selectors = document.getElementsByTagName("input");
-  for (let index = 0; index < selectors.length; index++) {    
-    if (selectors[index].checked) {
-      console.log(selectors[index].getAttribute("name"));
-        console.log(selectors[index].value);
-    }  
-  }  
+function filtrateAssignListener() {
+  var selectors = document.getElementsByTagName("input");
+  for (var index = 0; index < selectors.length; index++) {
+    selectors[index].addEventListener("change", function () {
+      if (this.checked) {
+        console.log(this);
+        var id = this.getAttribute("name");
+        switch (id) {
+          case "mazate":
+            console.log("mazate");
+            mazateVisible = true;
+            break;
+          case "coate":
+            console.log("coate");
+            break;
+          case "huitzilin":
+            console.log("huitzilin");
+            break;
+          case "michin":
+            console.log("michin");
+            break;
+        }
+      } else {
+        console.log("OFF");
+        var id = this.getAttribute("name");
+        switch (id) {
+          case "mazate":
+            console.log("mazate");
+            mazateVisible = false;
+            break;
+          case "coate":
+            console.log("coate");
+            break;
+          case "huitzilin":
+            console.log("huitzilin");
+            break;
+          case "michin":
+            console.log("michin");
+            break;
+        }
+      }
+    });
+  }
 }
+
 
 function drawPodium() {
   strokeWeight(10);
-  line(xMazate - (wMazate * escala) / 2, yMazate + (hMazate * escala) / 2, xMazate + (wMazate * escala) / 2, yMazate + (hMazate * escala) / 2);
-  line(xHuitzilin - (wHuitzilin * escala) / 2, yHuitzilin + (hHuitzilin * escala) / 2, xHuitzilin + (wHuitzilin * escala) / 2, yHuitzilin + (hHuitzilin * escala) / 2);
-  line(xCoate - (wCoate * escala) / 2, yCoate + (hCoate * escala) / 2, xCoate + (wCoate * escala) / 2, yCoate + (hCoate * escala) / 2);
-  line(xMichin - (wMichin * escala) / 2, yMichin + (hMichin * escala) / 2, xMichin + (wMichin * escala) / 2, yMichin + (hMichin * escala) / 2);
+  if (mazateVisible === true) {
+    line(xMazate - (wMazate * escala) / 2, yMazate + (hMazate * escala) / 2, xMazate + (wMazate * escala) / 2, yMazate + (hMazate * escala) / 2);
+  }
+  if (huitzilinVisible === true) {
+    line(xHuitzilin - (wHuitzilin * escala) / 2, yHuitzilin + (hHuitzilin * escala) / 2, xHuitzilin + (wHuitzilin * escala) / 2, yHuitzilin + (hHuitzilin * escala) / 2);
+  }
+  if (coateVisible === true) {
+    line(xCoate - (wCoate * escala) / 2, yCoate + (hCoate * escala) / 2, xCoate + (wCoate * escala) / 2, yCoate + (hCoate * escala) / 2);
+  }
+  if (michinVisible === true) {
+    line(xMichin - (wMichin * escala) / 2, yMichin + (hMichin * escala) / 2, xMichin + (wMichin * escala) / 2, yMichin + (hMichin * escala) / 2);
+  }
 }
 
 function drawScaleRanking() {
-  for (var i = 30; i >= 0; i -= 5) {
+  for (var i = 50; i >= 0; i -= 5) {
     textFont(font);
     fill(0).strokeWeight(0).textSize(25 * escala);
-    this.yval = map(i, 0, 30, windowHeight*0.6, 250 * escala);
+    this.yval = map(i, 0, 50, windowHeight * 0.6, 250 * escala);
     text((i) + "K", 10 * escala, yval);
     stroke('#CCC');
     strokeWeight(1);
@@ -162,14 +208,17 @@ function draw() {
   fill(255, 255, 255);
   stroke('#CCC');
   strokeWeight(10);
-  line(xCoate, yCoate, xCoate, windowHeight*0.6);
-  line(xHuitzilin, yHuitzilin, xHuitzilin, windowHeight*0.6);
-  line(xMazate, yMazate, xMazate, windowHeight*0.6);
-  line(xMichin, yMichin, xMichin, windowHeight*0.6);
-  //drawPodium();        
+  line(xCoate, yCoate, xCoate, windowHeight * 0.6);
+  line(xHuitzilin, yHuitzilin, xHuitzilin, windowHeight * 0.6);
+  line(xMazate, yMazate, xMazate, windowHeight * 0.6);
+  line(xMichin, yMichin, xMichin, windowHeight * 0.6);
+  //drawPodium();  
+
   image(imgCoate, xCoate, yCoate, wCoate * (escala / 1.5), hCoate * (escala / 1.5));
   image(imgHuitzilin, xHuitzilin, yHuitzilin, wHuitzilin * (escala / 2), hHuitzilin * (escala / 2));
+  if (mazateVisible === true){
   image(imgMazate, xMazate, yMazate, wMazate * (escala / 1.5), hMazate * (escala / 1.5));
+  }
   image(imgMichin, xMichin, yMichin, wMichin * (escala / 1.5), hMichin * (escala / 1.5));
   drawTitle();
   drawScore();
@@ -196,13 +245,13 @@ function drawTitle() {
     textFont(font);
     fill(0).strokeWeight(0).textSize(40 * escala);
     textAlign(CENTER);
-    text("CACAO APP\nPUNTAJE GENERAL", 0, 0, windowWidth, windowHeight*0.6 * 0.2);
+    text("CACAO APP\nPUNTAJE GENERAL", 0, 0, windowWidth, windowHeight * 0.6 * 0.2);
     textAlign(LEFT);
   }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight*0.6);
+  resizeCanvas(windowWidth, windowHeight * 0.6);
   setPositions();
   setScale();
 }
