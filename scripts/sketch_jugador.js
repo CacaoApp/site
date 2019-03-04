365b3e8ea9833c6cd1153ee4870f1368c3ed175a
Jose Andrés Moncada <josemoncada87@hotmail.com>
2019-03-04 18:17:50 -0500

    updating platform

---
+++ scripts/sketch_jugador.js
@@ -0,0 +1,34 @@
+var config = {
+  apiKey: "AIzaSyDj1Ao1CQpZa0t5UO2V1LhgCM6kqE1xPcA",
+  authDomain: "cacaoapp-f9f17.firebaseapp.com",
+  databaseURL: "https://cacaoapp-f9f17.firebaseio.com",
+  storageBucket: "cacaoapp-f9f17.appspot.com",
+  messagingSenderId: "390201177541"
+};
+
+firebase.initializeApp(config);
+
+var url = new URL(window.location.href);
+var identificador = url.searchParams.get("cid");
+if (identificador === null) {
+  console.log("URL ERROR");
+} else {
+  var refJugador = firebase.database().ref('estudiantes/' + identificador);
+  var nodeNombres = document.getElementById("contenedor_nombre_usuario");
+  var nodeApellidos = document.getElementById("contenedor_apellido_usuario");
+  var nodePuntos = document.getElementById("contenedor_puntos_usuario");
+  var nodeCacaos = document.getElementById("contenedor_cacaos_usuario");
+  var nodeGrupo = document.getElementById("contenedor_equipo_usuario");
+  refJugador.on('value', function (snapshot) {
+    var value = snapshot.val();
+    nodeNombres.innerText = value.nombres;
+    nodeApellidos.innerText = value.apellidos;
+    var refEquipoJugador = firebase.database().ref('grupos/' + value.grupo);
+    refEquipoJugador.on('value', function (snapshotGrupo) {
+      var valueGrupo = snapshotGrupo.val();
+      nodePuntos.innerText = valueGrupo.cantidadPuntos;
+      nodeCacaos.innerText = valueGrupo.cantidadCacaos;
+      nodeGrupo.innerText = valueGrupo.nombre;
+    });
+  });
+}
