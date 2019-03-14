53959bf8386a053050fa35ba80b68eedaf1ce344
Jose Andrés Moncada <josemoncada87@hotmail.com>
2019-03-14 09:22:51 -0500

    update for loading correct image on jugador team

--- scripts/sketch_jugador.js
+++ scripts/sketch_jugador.js
@@ -19,10 +19,34 @@ if (identificador === null) {
   var nodePuntos = document.getElementById("contenedor_puntos_usuario_span");
   var nodeCacaos = document.getElementById("contenedor_cacaos_usuario_span");
   var nodeGrupo = document.getElementById("contenedor_equipo_usuario_span");
+  var nodeImagenEquipo = document.getElementById("imagen_equipo_usuario");  
   refJugador.on('value', function (snapshot) {
     var value = snapshot.val();
     nodeNombres.innerText = value.nombres;
     nodeApellidos.innerText = value.apellidos;
+    let pathToImage = "assets/team_images/"
+    switch(value.grupo){
+      case "Coate":
+        pathToImage+="coate_team.png";
+      break;
+      case "Huitzilin":
+        pathToImage+="huitzilin_team.png";
+      break;
+      case "Mazate":
+        pathToImage+="mazate_team.png";
+      break;
+      case "Michin":
+        pathToImage+="michin_team.png";
+      break;
+      case "Ocelote":
+        pathToImage+="ocelote_team.png";
+      break;
+      case "Tlacuache":
+        pathToImage+="tlacuache_team.png";
+      break;      
+    }
+    nodeImagenEquipo.setAttribute("src", pathToImage );
+
     var refEquipoJugador = firebase.database().ref('grupos/' + value.grupo);
     refEquipoJugador.on('value', function (snapshotGrupo) {
       var valueGrupo = snapshotGrupo.val();
